7c1e97e0d04fb8e9239891fbf3c242b3f69ef99b
lcnde <176673026+lcnde@users.noreply.github.com>
2025-01-14 17:50:20 +0100

    fte: add tailwindcss support

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,7 +25,7 @@ const count = ref(0)
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
-  <p>
+  <p class="bg-red-500">
     Learn more about IDE Support for Vue in the
     <a
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
